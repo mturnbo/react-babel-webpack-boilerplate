03dd03572724b86c41f9be704c89b6a30e598a56
Marcus Turnbo <marcus@emarcus.com>
2017-08-23 17:57:32 -0500

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
