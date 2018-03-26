f91e0c2dafaffcc665d20ce69618cad1a762d18d
Marcus Turnbo <marcus@emarcus.com>
2018-03-26 07:59:18 -0500

    refactoring

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.scss';
 
-const Button = ({ label, onClick }) => {
-  return (
-    <button className="btn" onClick={onClick}>{label}</button>
-  );
-};
+const Button = ({ label, onClick }) => <button className="btn" onClick={onClick}>{label}</button>;
 
 Button.propTypes = {
   label: PropTypes.string.isRequired,
