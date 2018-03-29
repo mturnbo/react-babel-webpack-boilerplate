76affa6e69e3230689fafa32ffc5daedf6aedf43
Marcus Turnbo <marcus@emarcus.com>
2018-03-28 23:39:37 -0500

    Moved App to containers.  Added header and footer components.

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.scss';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="app-footer">
+        <h2>Footer</h2>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="app-header">
+        <h2>React Babel Webpack Boilerplate</h2>
+      </header>
+    );
+  }
+}
+
+export default Header;
